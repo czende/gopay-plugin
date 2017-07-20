fe6c5c7acb2a1c0efa50a04d195f87ddf7cd618a
Jan Czernin <jan.czernin@autodevelo.cz>
2017-07-20 20:15:12 +0200

    Dependency injection and some translations

---
+++ src/Action/ConvertPaymentAction.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BitBag\PayUPlugin\Action;
+
+use BitBag\PayUPlugin\OpenPayUWrapper;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\Model\PaymentInterface;
+use Payum\Core\Request\Convert;
+
+final class ConvertPaymentAction implements ActionInterface, GatewayAwareInterface
+{
+    use GatewayAwareTrait;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param Convert $request
+     */
+    public function execute($request)
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+
+        /**
+         * @var $payment PaymentInterface
+         */
+        $payment = $request->getSource();
+        $details = ArrayObject::ensureArrayObject($payment->getDetails());
+
+        $details['totalAmount'] = $payment->getTotalAmount();
+        $details['currencyCode'] = $payment->getCurrencyCode();
+        $details['extOrderId'] = uniqid($payment->getNumber());
+        $details['description'] = $payment->getDescription();
+        $details['client_email'] = $payment->getClientEmail();
+        $details['client_id'] = $payment->getClientId();
+        $details['customerIp'] = $this->getClientIp();
+        $details['status']  = OpenPayUWrapper::NEW_API_STATUS;
+
+        $request->setResult((array) $details);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($request)
+    {
+        return
+            $request instanceof Convert &&
+            $request->getSource() instanceof PaymentInterface &&
+            $request->getTo() === 'array'
+            ;
+    }
+
+    /**
+     * @return string|null
+     */
+    private function getClientIp()
+    {
+        return array_key_exists('REMOTE_ADDR', $_SERVER) ? $_SERVER['REMOTE_ADDR'] : null;
+    }
+}
---
+++ src/DependencyInjection/GoPayExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Czende\GoPayPlugin\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class GoPayExtension extends Extension {
+    
+    /**
+     * Load GoPay plugin extension YAML config
+     */
+    public function load(array $configs, ContainerBuilder $container) {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
