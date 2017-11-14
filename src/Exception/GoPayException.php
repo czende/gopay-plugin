c6da3fb148f8862512ffc0743028f5788a96c506
Jan Czernin <jan.czernin@gmail.com>
2017-11-14 15:53:15 +0100

    Added more informative error message

--- src/Exception/GoPayException.php
+++ src/Exception/GoPayException.php
@@ -7,10 +7,10 @@ use Payum\Core\Exception\Http\HttpException;
 class GoPayException extends HttpException {
     const LABEL = 'GoPayException';
 
-    public static function newInstance($status) {
-        $message = implode(PHP_EOL, [self::LABEL . ' ' . $status]);
+    public static function newInstance($status, $message = '') {
+        $output = implode(PHP_EOL, [self::LABEL . ' ' . $status . ' - ' . $message]);
 
-        $e = new static($message);
+        $e = new static($output);
 
         return $e;
     }
